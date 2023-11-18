69a8e45717c58d82ca9518276caa37f3cd76b011
tien-li <sjia13579@gmail.com>
2023-11-18 16:21:55 -0500

    mongodb upload

--- dataset/mongodb_upload.py
+++ dataset/mongodb_upload.py
@@ -1,129 +1,49 @@
 import json
+import os
 
-from mongoengine import connect
+file_path = set()
+count = 0
 
-connect(db='data', host='mongodb+srv://tienli:Jz1JvtQMMqGjLKVY@restaurant.yx3ut6b.mongodb.net/?retryWrites=true&w=majority')
-from mongoengine import Document, StringField, ListField, DictField
-
-from mongoengine import Document, StringField, FloatField, IntField, ListField, DictField, EmbeddedDocumentField, EmbeddedDocument
-
-# Define a sub-document for reviews
-class Review(EmbeddedDocument):
-    id = IntField(required=True)
-    text = StringField(required=True)
-
-# Define the main Restaurant document
-class Restaurant(Document):
-    business_id = StringField(required=True)
-    name = StringField(required=True)
-    address = StringField()
-    city = StringField()
-    state = StringField()
-    postal_code = StringField()
-    latitude = FloatField()
-    longitude = FloatField()
-    stars = FloatField()
-    review_count = IntField()
-    is_open = IntField()
-    attributes = DictField()
-    categories = StringField()
-    hours = DictField()
-    reviews = ListField(EmbeddedDocumentField(Review))
-
-    # Additional methods can be defined here as needed
-
-
-# Replace 'category' with your specific category
-with open('categories/Breakfast_data.json', 'r') as file:
-    restaurants_data = json.load(file)
+categories = ['Breakfast', 'Bubble', 'Burger', 'Cafe', 'Chinese', 'Indian', 'Italian', 'Japanese', 'Korean', 'Mexican', 'Pizza', 'Vietnamese']
 
 # This will store combined data
 combined_data = []
 
-for restaurant in restaurants_data:
+for category in categories:
+    # Replace 'category' with your specific category
+    with open(f'categories/{category}_data.json', 'r') as file:
+        restaurants_data = json.load(file)
 
-    business_id = restaurant['business_id']
+    for restaurant in restaurants_data[:10]:
 
-    # Open the corresponding reviews file
-    # Replace 'business_id' with the actual ID from the data
-    try:
-        with open(f'Breakfast/{business_id}_reviews.json', 'r') as review_file:
-            reviews = json.load(review_file)
-            restaurant['reviews'] = reviews  # Combine the data
-            combined_data.append(restaurant)
-    except FileNotFoundError:
-        print(f"No review file found for business ID: {business_id}")
-    break
-# Now combined_data contains all the information
+        business_id = restaurant['business_id']
 
-for data in combined_data:
-    # Create Review embedded documents for each review
-    review_objects = [Review(id=review['id'], text=review['text']) for review in data['reviews']]
+        # Open the corresponding reviews file
+        # Replace 'business_id' with the actual ID from the data
+        try:
+            with open(f'{category}/{business_id}_reviews.json', 'r') as review_file:
+                reviews = json.load(review_file)
+                restaurant['reviews'] = reviews  # Combine the data
+                if f'{business_id}_reviews.json' not in file_path:
+                    combined_data.append(restaurant)
+                    file_path.add(f'{business_id}_reviews.json')
+                    count += 1
+                
+        except FileNotFoundError:
+            print(f"No review file found for business ID: {business_id}")
 
-    # Create a Restaurant document with all fields from the data
-    restaurant = Restaurant(
-        business_id=data['business_id'],
-        name=data['name'],
-        address=data.get('address', ''),
-        city=data.get('city', ''),
-        state=data.get('state', ''),
-        postal_code=data.get('postal_code', ''),
-        latitude=data.get('latitude', 0),
-        longitude=data.get('longitude', 0),
-        stars=data.get('stars', 0),
-        review_count=data.get('review_count', 0),
-        is_open=data.get('is_open', 0),
-        attributes=data.get('attributes', {}),
-        categories=data.get('categories', ''),
-        hours=data.get('hours', {}),
-        reviews=review_objects
-    )
+print(count)
+print(len(file_path))
 
-    # Save the document to MongoDB
-    restaurant.save()
+from pymongo import MongoClient
+# Connect to your MongoDB instance
+client = MongoClient('mongodb+srv://tienli:Jz1JvtQMMqGjLKVY@restaurant.yx3ut6b.mongodb.net/?retryWrites=true&w=majority', tls=True, tlsAllowInvalidCertificates=True)
 
+# # Select your database
+db = client['data']
 
+# Select your collection
+collection = db['Restaurant']
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# print(combined_data[0])
-# from pymongo import MongoClient
-
-# # Connect to your MongoDB instance
-# client = MongoClient('mongodb+srv://tienli:Jz1JvtQMMqGjLKVY@restaurant.yx3ut6b.mongodb.net/?retryWrites=true&w=majority', tls=True, tlsAllowInvalidCertificates=True)
-
-# databases = client.list_database_names()
-# print("Databases:", databases)
-# # # Select your database
-# database = client['test_database']
-# collection = database['test_collection']
-# a = {'name': 'a', 'gender': 'male'}
-# b = {'name': 'b', 'gender': 'male'}
-# c = {'name': 'c', 'gender': 'female'}
-
-# collection.insert_one(a)       # 插入一個檔案
-# collection.insert_many([b, c]) # 插入多個檔案
-# db = client['data']
-
-# # Select your collection
-# collection = client['restaurant']
-# print(collection)
-
-
-# # Insert data into the collection
-# collection.insert_many(combined_data)
+# Insert data into the collection
+collection.insert_many(combined_data)
