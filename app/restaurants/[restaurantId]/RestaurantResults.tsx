914766be03930d812c3d653a477d055b4c92c8e6
Reggie Hsu <reggiehsu111@gmail.com>
2023-11-19 02:44:49 -0500

    fja

--- app/restaurants/[restaurantId]/RestaurantResults.tsx
+++ app/restaurants/[restaurantId]/RestaurantResults.tsx
@@ -6,32 +6,40 @@ interface RestaurantResultsProps {
   Score: number | null;
 }
 
-const RestaurantResults: React.FC<RestaurantResultsProps> = ({ results }) => {
-  if (!results) {
-    return <div>No results available.</div>;
-  }
-
+const RestaurantResults: React.FC<RestaurantResultsProps> = ({
+  Positive,
+  Negative,
+  Score,
+}) => {
   return (
     <div className="grid grid-cols-2 gap-4 h-96">
       <div className="overflow-y-auto">
-        {results.Positive.map((result) => (
-          <div
-            key={result}
-            className="col-span-1 p-10 my-4 bg-green-100 rounded-lg"
-          >
-            {result}
-          </div>
-        ))}
+        {!Positive && (
+          <div className="text-center text-lg"> No Results Found</div>
+        )}
+        {Positive &&
+          Positive.map((result) => (
+            <div
+              key={result}
+              className="col-span-1 p-10 my-4 bg-green-100 rounded-lg"
+            >
+              {result}
+            </div>
+          ))}
       </div>
       <div className="overflow-y-auto">
-        {results.Negative.map((result) => (
-          <div
-            key={result}
-            className="col-span-1 p-10 my-4 bg-red-100 rounded-lg"
-          >
-            {result}
-          </div>
-        ))}
+        {!Negative && (
+          <div className="text-center text-lg"> No Results Found</div>
+        )}
+        {Negative &&
+          Negative.map((result) => (
+            <div
+              key={result}
+              className="col-span-1 p-10 my-4 bg-red-100 rounded-lg"
+            >
+              {result}
+            </div>
+          ))}
       </div>
     </div>
   );
